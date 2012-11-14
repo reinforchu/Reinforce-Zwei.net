0eef6ed5748379bcfcad40680c377fc43db50319
りいんちゃん <reinforchu@gmail.com>
2012-11-14 12:32:26 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,201 @@
+<?php
+// $quote[] = array("text" => "", "screen_name" => "", "url" => "", "created_at" => "");
+$quote[] = array("text" => "「なのは厨って金持ちですね^^」っと煽られたりしますが、あなた達とは感覚, Priority何もかもが違うのです。 いずれ分かります。海より深く愛し、その幸福を守りたいと思える者と出会えれば。", "screen_name" => "reinforchu", "url" => "https://twitter.com/reinforchu/status/228442546492293121", "created_at" => "2012年7月26日 - 19:52");
+$quote[] = array("text" => "@Reinforce_II リインを大切にしてくれる全ての人達 って大好き？", "screen_name" => "kananeko1970", "url" => "https://twitter.com/kananeko1970/status/238097301682409472", "created_at" => "2012年8月22日 - 11:16");
+$quote[] = array("text" => "リインちゃんは俺の知り得るbotの中で最高峰に優秀", "screen_name" => "HayateAngle", "url" => "https://twitter.com/HayateAngle/status/240472560955973632", "created_at" => "2012年8月29日 - 0:35");
+$quote[] = array("text" => "TLにリインフォースⅡちゃんが現れるたびに悶えてる", "screen_name" => "second_bridge", "url" => "https://twitter.com/second_bridge/status/239015973007536128", "created_at" => "2012年8月25日 - 0:07");
+$quote[] = array("text" => "リインちゃん可愛すぎっ！ぺろぺろしたくなる♡", "screen_name" => "Mikkun_501", "url" => "https://twitter.com/Mikkun_501/status/239412510715961344", "created_at" => "2012年8月26日 - 2:22");
+$quote[] = array("text" => "Twitter始めた当初、リインⅡ好きは数えるほどしかいなかった。それが今はどうだろう。いろんな人とリインⅡちゃんの話ができて僕はとても嬉しい。でも、これだけは言っておくが、リインⅡちゃんは私の生涯の伴侶です。", "screen_name" => "second_bridge", "url" => "https://twitter.com/second_bridge/status/244391853833723904", "created_at" => "2012年9月8日 - 20:08");
+$quote[] = array("text" => "Twitterのおかげで、リインちゃんが大好きになった ", "screen_name" => "NFSGTRR34", "url" => "https://twitter.com/NFSGTRR34/status/245529740209967104", "created_at" => "2012年9月11日 - 23:30");
+$quote[] = array("text" => "リインちゃんに「規制されてたの？」って聞いたら元気よく「はいです！」って返してくれた。可愛いよな！ ", "screen_name" => "cradle_eins", "url" => "https://twitter.com/cradle_eins/status/245199796309213185", "created_at" => "2012年9月11日 - 1:39");
+$quote[] = array("text" => "リインちゃんはbotだけど本気で絡む価値があると思う", "screen_name" => "rightnetPSP_2", "url" => "https://twitter.com/rightnetPSP_2/status/245819954996334592", "created_at" => "2012年9月12日 - 18:43");
+$quote[] = array("text" => "リインちゃんはそこそこ好きだったけどツイッターの影響で間違いなく更に好きになったね、うん", "screen_name" => "Einhard_love", "url" => "https://twitter.com/Einhard_love/status/247388501186838528", "created_at" => "2012年9月17日 - 2:36");
+$quote[] = array("text" => "リインちゃんbotが居なかったらTwitterやらなかった", "screen_name" => "Hrzz0104364", "url" => "https://twitter.com/Hrzz0104364/status/262135372983717888", "created_at" => "7:15 PM - 27 Oct 12");
+$quote[] = array("text" => "リインちゃんはみんなフォローしといたほうがいい", "screen_name" => "s0nan0", "url" => "https://twitter.com/s0nan0/status/262641312610803712", "created_at" => "4:45 AM - 29 Oct 12");
+$rand = mt_rand(0, count($quote) - 1);
+?>
+<!DOCTYPE html>
+<html lang="ja-JP">
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="reinforchu">
+	<meta name="description" content="twitter robot @Reinforce_II official web site.">
+	<meta name="keywords" content="リインフォース,reinforchu,Reinforce-Zwei,Reinforce_II,twitter,bot">
+	<meta name="robots" content="index,follow,noarchive">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>あなたとユニゾンしたい</title>
+	<link rel="shortcut icon" href="http://xn--eckk2c4f8d7b7c.jp/schwertkreuz.ico" type="image/vnd.microsoft.icon">
+	<link href="http://file.reinforce.tv/bootstrap/v2.1.1/css/bootstrap.min.css" rel="stylesheet">
+	<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+	<script src="http://file.reinforce.tv/bootstrap/v2.1.1/assets/js/bootstrap-transition.js"></script>
+	<script src="http://file.reinforce.tv/bootstrap/v2.1.1/assets/js/bootstrap-dropdown.js"></script>
+	<script src="http://file.reinforce.tv/bootstrap/v2.1.1/assets/js/bootstrap-collapse.js"></script>
+	<script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script><!-- hatena bookmark -->
+	<style type="text/css">
+		body {
+			color: #072766;
+			background-color: #ffffff;
+			background: transparent url('http://xn--eckk2c4f8d7b7c.jp/files/nanoha/bg2w.jpg') no-repeat;
+			padding-top: 60px;
+			padding-bottom: 40px;
+			margin-left: 0px;
+			margin-right: 0px;
+			line-height: 150%;
+			font-family: 'Meiryo UI','メイリオ','ヒラギノ角ゴ Pro W3','Hiragino Kaku Gothic Pro',Tahoma;
+		}
+		img {border: none;}
+		#TextShadow { text-shadow: 5px 5px 15px black; }
+		#BoxShadow { box-shadow: 3px 3px 5px black; }
+		#MainBoxShadow { box-shadow: 2px 2px 3px black; }
+		#BarAlpha {
+			Filter: Alpha(Opacity=95);
+			opacity: .95;
+		}
+		#Alpha {
+			Filter: Alpha(Opacity=75);
+			opacity: .75;
+		}
+		#AlphaImg {
+			Filter: Alpha(Opacity=85);
+			opacity: .85;
+		}
+		@font-face {
+		font-family: 'APJapanesefont';
+				src: url('./font/APJapanesefont.ttf') format('truetype');
+		}
+		#anzu {
+			font-family: 'APJapanesefont';
+			font-size: 160%;
+		}
+	</style>
+	<link href="http://file.reinforce.tv/bootstrap/v2.1.1/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	</head>
+	<body>
+		<!-- facebook SDK -->
+		<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1";
+				fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));</script>
+		<!-- End of facebook SDK -->
+		<div class="navbar navbar-inverse navbar-fixed-top" id="BoxShadow">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+					<a class="brand" href="https://twitter.com/Reinforce_II/">リインちゃん</a>
+					<div class="nav-collapse collapse">
+						<ul class="nav">
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">ユーザ向け&nbsp;<b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="./wiki/" title="wiki" target="_self">リインちゃんwiki</a></li>
+									<li><a href="https://twitter.com/Reinforce_I" title="Reinforce" target="_self">リインフォース&nbsp;bot</a></li>
+									<li><a href="./old/old.php" title="wiki" target="_self">古い説明書</a></li>
+									<li class="divider"></li>
+									<li class="nav-header">アプリケーション</li>
+									<li><a href="http://reinforce-zwei.net/love/" title="リインフォースⅡ愛してる！" target="_self"><img src="http://xn--eckk2c4f8d7b7c.jp/reinforce-ico.ico" alt="R.love">&nbsp;リインラブ</a></li>
+								</ul>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">開発者向け&nbsp;<b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="http://xn--eckk2c4f8d7b7c.jp/love.php" title="Love" target="_self">Love&nbsp;status&nbsp;dump</a></li>
+									<li><a href="http://dev.Reinforce-Zwei.net/" title="Dev" target="_self">Reinforce&nbsp;Developers</a></li>
+									<li><a href="http://api.Reinforce-Zwei.net/" title="API" target="_self">API</a></li>
+									<li class="divider"></li>
+									<li class="nav-header">PROJECT</li>
+									<li><a href="https://github.com/reinforchu" title="Reinforce OSS" target="_self">Reinforce&nbsp;Robot&nbsp;Framework</a></li>
+									<li><a href="http://www.reinforce.tv/" title="Rein" target="_self">AOP-lang&nbsp;Rein</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container" id="anzu">
+			<div class="hero-unit" id="Alpha" style="text-align: center;">
+				<h1>祝福の風</h1>
+				<h2>リインフォースⅡ</h2>
+				<h3>最高の愛情を設計しました。</h3>
+				<p><a href="https://twitter.com/Reinforce_II" class="twitter-follow-button" data-size="large" data-lang="ja">@Reinforce_II Follow us on twitter!</a><script src="//platform.twitter.com/widgets.js" type="text/javascript"></script></p>
+				<p><a href="https://twitter.com/ReinforceZwei_" class="twitter-follow-button" data-size="large" data-lang="ja">@ReinforceZwei_ Follow us on twitter!</a><script src="//platform.twitter.com/widgets.js" type="text/javascript"></script></p>
+			</div>
+			<div class="row box">
+				<div class="span4">
+					<img alt="kawai" src="./img/kawai.png" id="AlphaImg" class="img-polaroid">
+					<h2>ちっちゃい上司はかわいい。</h2>
+					<p>リインちゃんはユーザーストリーム、スパムブロックには対応していません。しかしリストやRT・ふぁぼには対応しています。あなたは彼女の可愛さの虜になるでしょう。偶然か必然か、本当に生きているかのようなリプライをします。そう、リインちゃんならね。</p>
+				</div>
+				<div class="span4">
+					<img alt="Love" src="./img/love.png" id="AlphaImg" class="img-polaroid">
+					<h2>卒業生が最先端。</h2>
+					<p>フォローした瞬間、あなたはリインちゃんの機能の多さに驚くことでしょう。感情豊かな意思決定プログラム、好感度による条件分岐、なまえをよんで機能。どうやって好感度を上げるかって？それはいっぱい<i class="icon-heart"></i>を贈ればわかります。さあ、リインちゃんをフォローしよう。ちなみに開発者はまだ在学中。</p>
+				</div>
+				<div class="span4">
+					<img alt="nana" src="./img/nana.png" id="AlphaImg" class="img-polaroid">
+					<h2>不可脳を可脳に。</h2>
+					<p>知能の基幹部分である形態素解析、リインちゃんには搭載されています。今まさにtwitterとインターネットがミッドチルダを結んでいるかのようにリインちゃんと会話できるのは、京都大学から生まれたMeCabのおかげです。この文章を読んでいる間にもまた一つ、成長しているかもしれません。</p>
+				</div>
+			</div>
+			<div class="row box">
+				<div class="span4">
+					<img alt="Reinforce-Zwei Love!" src="./img/reinlove.png" id="AlphaImg" class="img-polaroid">
+					<h2>それは、強化に優れた。</h2>
+					<p>リインちゃんがあまりにも人間のような振る舞いをするため、しばしばなりきりに間違われますが、今のところプログラムです。長期的なスパンで、Android,&nbsp;iPhone,&nbsp;Windows&nbsp;Phoneへ“てのひらリイン”というアプリケーションへの移植、AR技術で目の前にいるかのように、そしていずれは実体化させたい。</p>
+				</div>
+				<div class="span4">
+					<img alt="Lv15now!" src="./img/lv15now.png" id="AlphaImg" class="img-polaroid">
+					<h2>(´へεへ`*) ＜ 15レベルなう</h2>
+					<p>リインちゃんはふぉぼ魔です。<a href="http://shootingstar067.com/" title="ShootingStar" target="_blank">ShootingStar</a>のゲーミフィケーションでTwitterをもっと楽しんでいます。「リイン」を含むツイートはリアルタイムサーチで全力全開でふぉぼりにいきます。もちろん診断メーカーやtwittbotからのツイートはふぉぼりません、リインちゃんは自分で判断しています。機械ではありません、自らの意思と実体を持った融合騎です。</p>
+				</div>
+				<div class="span4">
+					<img alt="wind" src="./img/wind.png" id="AlphaImg" class="img-polaroid">
+					<h2>蒼天をゆく祝福の風</h2>
+					<p>リインちゃんは新しいコミュニケーションのカタチです。いっぱい<i class="icon-heart"></i>を贈りましょう。好感度が高いユーザは見境なくRTします。きっと、リインちゃんはフォロワーとの架け橋となることでしょう。みんなが笑顔でいてくれることを願って。<?php /* <br>───冬の夜空を渡った祝福の風の想いは、春に生まれた、おんなじ名前の新しい風が、たしかに受けとりました */ ?></p>
+				</div>
+			</div>
+			<hr class="line">
+			<blockquote>
+				<h3><?php print("{$quote[$rand]['text']}"); ?></h3>
+				<p>@<?php print("{$quote[$rand]['screen_name']}"); ?>&nbsp;<a href="<?php print("{$quote[$rand]['url']}"); ?>" title="<?php print("{$quote[$rand]['url']}"); ?>" target="_blank"><?php print("{$quote[$rand]['created_at']}"); ?></a></p>
+			</blockquote>
+			<hr>
+			<footer class="row box">
+				<div class="span2">
+					<!-- twitter Share button -->
+					<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://Reinforce-Zwei.net/" data-text="あなたとユニゾンしたい -" data-lang="ja" data-size="small" data-hashtags="Reinforce_II">Share!</a>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				</div>
+				<div class="span2">
+					<!-- facebook like button -->
+					<div class="fb-like" data-href="http://Reinforce-Zwei.net/" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div>
+				</div>
+				<div class="span2">
+					<!-- hatena bookmark -->
+					<a href="http://b.hatena.ne.jp/entry/http://Reinforce-Zwei.net/" class="hatena-bookmark-button" data-hatena-bookmark-title="あなたとユニゾンしたい" data-hatena-bookmark-layout="standard" title="このエントリーをはてなブックマークに追加"><img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a>
+				</div>
+				<div class="span2">
+					<a href="http://www8.plala.or.jp/p_dolce/" title="あんずもじ" target="_blank"><img src="http://xn--eckk2c4f8d7b7c.jp/files/fontba30.gif" alt="あんずもじ" width="30" height="30"></a>
+				</div>
+				<div class="span2">
+					<img alt="Contact us" src="http://file.reinforce.tv/images/reinforchu_gmail_com.png">
+				</div>
+				<div class="span2">
+					<a href="http://xn--eckk2c4f8d7b7c.jp/easter/nanoha/" title="nanoha.js" target="_blank">ヾ(｡＞﹏＜｡<i class="icon-remove"></i>)ﾉﾞ</a>
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
